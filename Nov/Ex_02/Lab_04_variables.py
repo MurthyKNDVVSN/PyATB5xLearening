6855a1f47055f1cdd7161aaabcc89aa9513d821f
MURTHY <satya.sistikarna@gmail.com>
2025-11-01 07:58:37 +0530

    Initial commit

---
+++ Nov/Ex_02/Lab_04_variables.py
@@ -0,0 +1,14 @@
+ # to see all the keyword
+# import keyword
+# print(keyword.kwlist)
+
+#  ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue',
+ #  'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
+ #  'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+ #
+ # Process finished with exit code 0
+
+ # variable_name = variable_value
+age = 89
+age = 548
+print(age)
---
+++ Nov/Ex_02/lab_Var_Types_rules.py
@@ -0,0 +1,3 @@
+age = 65 # intergers - int
+pi = 3.14 # float - decimal value
+name = "ROGGER ROGGER"   # string bunch of characters - Str
